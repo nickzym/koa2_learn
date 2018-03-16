505252061a36138dd41071ac4d5bf1fdeedcb55e
nickzym <yimingzh@usc.edu>
2018-03-16 18:40:12 +0800

    add ApiError

--- app/middleware/response_formatter.js
+++ app/middleware/response_formatter.js
@@ -1,3 +1,5 @@
+var ApiError = require("../error/ApiError");
+
 var response_formatter = async ctx => {
     if (ctx.body) {
         ctx.body = {
@@ -18,7 +20,20 @@ var url_filter = pattern => {
         var reg = new RegExp(pattern);
 
         //go first execute router funct
-        await next();
+        try {
+            await next();
+        } catch (err) {
+            // deal with all excecption here happend in routes or middlewares
+            if (err instance of ApiError && reg.test(ctx.originalUrl)) {
+                ctx.status = 200;
+                ctx.body = {
+                    code: error.code,
+                    message: error.message
+                }
+            }
+            //continue throw error for log to cacth
+            throw err;
+        }
 
         if(reg.test(ctx.originalUrl)) {
             response_formatter(ctx);
